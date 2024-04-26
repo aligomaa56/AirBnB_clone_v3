260387453543c24f5b5519c7cc416dca87bfe91b
aligomaa56 <3looosh56565656@gmail.com>
2024-04-27 00:08:41 +0300

    task-4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,35 @@
 """index file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {
+    "users": User,
+    "places": Place,
+    "states": State,
+    "cities": City,
+    "amenities": Amenity,
+    "reviews": Review
+}
 
 
 @app_views.route('/status')
 def status():
     ''' routes to status page '''
-    status = {
-        'status': 'OK'
-    }
+    status = {'status': 'OK'}
     return jsonify(status)
+
+
+@app_views.route('/stats')
+def count_stats():
+    """Count stats"""
+    counter_of_stats = {}
+    for class_name, class_ in classes.items():
+        counter_of_stats[class_name] = storage.count(class_)
+    return jsonify(counter_of_stats)
