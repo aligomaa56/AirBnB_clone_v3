c360732be2eb3cae35eee8cccef92e129489694c
aligomaa56 <3looosh56565656@gmail.com>
2024-04-28 18:41:04 +0300

    task-12/fix-1

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
